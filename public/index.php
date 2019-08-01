958b35cfe9ca91cf502ff0f46a6eacb1292d74ed
KingFox <rune.evo2012@gmail.com>
2019-08-01 03:56:57 -0500

    added basic headers

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+    header("X-XSS-Protection: 1; mode=block");
+    header('X-Content-Type-Options: nosniff');
+    header("Strict-Transport-Security:max-age=63072000");
+
 	use Phalcon\Loader;
 	use Phalcon\Mvc\View;
 	use Phalcon\Mvc\Application;
