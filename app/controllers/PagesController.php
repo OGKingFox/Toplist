37c9a96cef054910232a4e4a5fdd86f69efffef8
KingFox <rune.evo2012@gmail.com>
2019-08-02 03:18:09 -0500

    minor seo optimization

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -4,6 +4,9 @@ class PagesController extends BaseController {
 
     public function docsAction() {
         $this->tag->setTitle(ucwords($this->router->getActionName()));
+
+        $this->view->description = "Documentation on how to get set up using our toplist to it's fullest, 
+            including code examples and detailed guides.";
     }
 
 }
