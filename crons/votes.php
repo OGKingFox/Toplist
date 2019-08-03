d3b01ae59d17df4fd3ea01c24d31657e698a48ba
KingFox <rune.evo2012@gmail.com>
2019-08-03 01:20:34 -0500

    updated cron files

--- crons/votes.php
+++ crons/votes.php
@@ -1,17 +1,7 @@
 <?php
-    include '../public/config.php';
+    include 'db_connect.php';
 
     $log_file = "cron.log";
-
-    try {
-        $pdo = new PDO("mysql:host=" . host . ";dbname=" . dbname, username, password);
-        $pdo->exec("SET CHARACTER SET utf8");
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    } catch (PDOException $e) {
-        error_log(date('[m-d-y g:i A] ')."[Error] Connection failed: {$e->getMessage()} ".PHP_EOL, 3, $log_file);
-        exit;
-    }
 
     $start = microtime(true);
     $stmt = $pdo->prepare("UPDATE servers SET votes = (SELECT COUNT(*) FROM votes WHERE votes.server_id = servers.id)");
