a9e3c211847ebefa51c9587aa383782adc048ee3
KingFox <rune.evo2012@gmail.com>
2019-08-03 01:22:30 -0500

    updated cron files again.... :facepalm:

--- crons/votes.php
+++ crons/votes.php
@@ -1,8 +1,6 @@
 <?php
     include 'db_connect.php';
 
-    $log_file = "cron.log";
-
     $start = microtime(true);
     $stmt = $pdo->prepare("UPDATE servers SET votes = (SELECT COUNT(*) FROM votes WHERE votes.server_id = servers.id)");
     $stmt->execute();
@@ -12,5 +10,5 @@
     $elapsed = number_format($end - $start, 4);
 
     echo "[Success] Updated $updated Records in ".$elapsed."s! ";
-    error_log(date('[m-d-y g:i A] ')."[Success] Updated $updated Records! ".PHP_EOL, 3, $log_file);
+    error_log(date('[m-d-y g:i A] ')."[Success] Updated $updated Records in ".$elapsed."s! ".PHP_EOL, 3, log_file);
 ?>
