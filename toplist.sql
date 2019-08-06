dbb4986a3eed7c9a1d6fd1da7d41834667e035c8
KingFox <rune.evo2012@gmail.com>
2019-08-05 19:03:44 -0500

    updated sql file

--- toplist.sql
+++ toplist.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 02, 2019 at 08:45 PM
--- Server version: 10.1.39-MariaDB
--- PHP Version: 7.1.29
+-- Host: localhost
+-- Generation Time: Aug 05, 2019 at 06:02 PM
+-- Server version: 5.7.27-0ubuntu0.16.04.1
+-- PHP Version: 7.0.33-0ubuntu0.16.04.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,6 +23,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `server_id` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `comment` text NOT NULL,
+  `date_posted` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `games`
 --
 
@@ -34,17 +47,71 @@ CREATE TABLE `games` (
   `enabled` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `games`
+-- Table structure for table `images`
 --
 
-INSERT INTO `games` (`id`, `title`, `enabled`) VALUES
-(1, 'Runescape Private Server', 1),
-(2, 'Minecraft', 1),
-(3, 'DayZ', 1),
-(4, 'Rust', 1),
-(5, 'Battlefield 4', 1),
-(6, 'Battlefield 1', 1);
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `server_id` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `server_id` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL DEFAULT '-1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+
+CREATE TABLE `packages` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `base_price` float NOT NULL,
+  `length` int(11) NOT NULL DEFAULT '0',
+  `features` text,
+  `active` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `packages`
+--
+
+INSERT INTO `packages` (`id`, `title`, `price`, `base_price`, `length`, `features`, `active`) VALUES
+(1, 'Level 1 | 30 Days Premium', 9.99, 4.99, 2592000, '["+100 Instant Votes", null, "Animated Banner", "Highlighted Background", "Applies to all servers"]', 1),
+(2, 'Level 2 | 60 Days Premium', 19.99, 19.99, 5184000, '["+200 Instant Votes", "+200 Votes every reset", "Animated Banner", "Highlighted Background", "Applies to all servers"]', 1),
+(3, 'Level 3 | 90 Days Premium', 29.99, 29.97, 7776000, '["+300 Instant Votes", "+300 Votes every reset", "Animated Banner", "Highlighted Background", "Applies to all servers"]', 1),
+(5, 'Level 4 | 180 Days Premium', 49.99, 49.95, 15552000, '["+500 Instant Votes", "+500 Votes every reset", "Animated Banner", "Highlighted Background", "Applies to all servers"]', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `server_id` int(11) NOT NULL,
+  `reason` text NOT NULL,
+  `date_submitted` bigint(20) NOT NULL DEFAULT '-1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -56,13 +123,18 @@ CREATE TABLE `servers` (
   `id` int(11) NOT NULL,
   `owner_id` bigint(20) NOT NULL,
   `owner_tag` varchar(255) NOT NULL,
+  `votes` int(11) NOT NULL DEFAULT '0',
   `game` varchar(255) DEFAULT NULL,
   `title` varchar(255) NOT NULL,
   `website` varchar(255) DEFAULT NULL,
   `callback` varchar(255) DEFAULT NULL,
-  `discord_id` bigint(20) DEFAULT '-1',
-  `summary` varchar(255) DEFAULT NULL,
-  `info` longtext
+  `discord_id` varchar(255) DEFAULT NULL,
+  `banner_url` varchar(255) DEFAULT 'img/banner_light.png',
+  `meta_info` varchar(255) DEFAULT NULL,
+  `meta_tags` text,
+  `info` longtext,
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `date_created` bigint(20) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -74,26 +146,72 @@ CREATE TABLE `servers` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `verified` tinyint(1) NOT NULL,
-  `locale` varchar(255) NOT NULL,
-  `premium_type` int(11) NOT NULL,
-  `mfa_enabled` tinyint(1) NOT NULL,
-  `user_id` bigint(20) NOT NULL,
-  `flags` int(11) NOT NULL,
-  `avatar` varchar(255) NOT NULL,
-  `discriminator` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `premium_expires` bigint(20) DEFAULT NULL,
+  `premium_level` int(11) NOT NULL DEFAULT '0',
+  `premium_type` int(11) NOT NULL DEFAULT '-1',
+  `verified` tinyint(1) NOT NULL DEFAULT '1',
+  `locale` varchar(255) NOT NULL DEFAULT 'en-US',
+  `mfa_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `user_id` bigint(20) DEFAULT NULL,
+  `flags` int(11) NOT NULL DEFAULT '-1',
+  `avatar` varchar(255) DEFAULT NULL,
+  `discriminator` int(11) NOT NULL DEFAULT '-1',
+  `email` varchar(255) DEFAULT NULL,
   `role` varchar(255) NOT NULL DEFAULT 'member'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `id` int(11) NOT NULL,
+  `server_id` varchar(255) NOT NULL,
+  `ip_address` varchar(255) DEFAULT NULL,
+  `incentive` varchar(255) DEFAULT NULL,
+  `voted_on` bigint(20) NOT NULL DEFAULT '-1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `games`
 --
 ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `packages`
+--
+ALTER TABLE `packages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -109,28 +227,60 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `games`
 --
 ALTER TABLE `games`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `packages`
+--
+ALTER TABLE `packages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `servers`
 --
 ALTER TABLE `servers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `votes`
+--
+ALTER TABLE `votes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=304;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
