5d2229df3da6328736bcb9e91c25d23517c9ab3f
KingFox <rune.evo2012@gmail.com>
2019-08-16 23:38:31 -0500

    removed statistics on server page above footer, moving elsewhere

--- app/controllers/ServersController.php
+++ app/controllers/ServersController.php
@@ -233,6 +233,136 @@ class ServersController extends BaseController {
         return true;
     }
 
+    public function discordAction() {
+        $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
+
+        if (!$this->request->isPost() ||! $this->request->isAjax()) {
+            return false;
+        }
+
+        $server_id = $this->request->getPost("server_id", "int");
+
+        $discord = new Discord($server_id);
+        $discord->fetch();
+
+        $data = $discord->getRawData();
+
+        if (isset($data->code)) {
+            $this->flash->error("Error loading Discord: ".$data->message);
+        } else {
+            $this->view->discord = $discord;
+        }
+        return true;
+    }
+
+    public function reportAction() {
+        if (!$this->request->isPost() /*|| !$this->security->checkToken()*/) {
+            return $this->response->redirect("");
+        }
+
+        $user_id  = $this->getUser() ? $this->getUser()->id : null;
+        $username = $this->getUser() ? $this->getUser()->username : null;
+        $serverId = $this->request->getPost("serverId", "int");
+        $comment  = nl2br($this->request->getPost("comment", 'string'));
+
+        $server = Servers::getServer($serverId);
+
+        if (!$server) {
+            $this->dispatcher->forward([
+                'controller' => 'errors',
+                'action' => 'show404'
+            ]);
+            return true;
+        }
+
+        $this->view->seo_title = Servers::genSeoTitle($server);
+        $this->view->server = $server;
+
+        $lastReport = Reports::getRecentReport($user_id, $serverId);
+
+        if ($lastReport) {
+            $this->view->saved = false;
+            $this->view->error = "You have already submitted a report on this server within the last 5 minutes. Take a chill pill.";
+            return true;
+        }
+
+        $report = new Reports;
+        $report->setUserId($user_id);
+        $report->setUsername($username);
+        $report->setServerId($server->id);
+        $report->setReason($comment);
+        $report->setDateSubmitted(time());
+
+        if ($report->save()) {
+            $this->view->saved = true;
+        } else {
+            $this->view->saved = false;
+            $this->view->error = $report->getMessages()[0];
+        }
+    }
+
+    public function likeAction() {
+        $this->view->setRenderLevel(View::LEVEL_NO_RENDER);
+
+        if (!$this->request->isAjax()) {
+            $this->println([
+                'success' => false,
+                'message' => 'This page is available via ajax only!'
+            ]);
+            return false;
+        }
+
+        $serverId = $this->request->getPost("id", "int");
+        $server   = Servers::getServer($serverId);
+
+        if (!$server) {
+            $this->println([
+                'success' => false,
+                'message' => 'This server does not exist.'
+            ]);
+            return false;
+        }
+
+        $user_id = $this->getUser() ? $this->getUser()->id : -1;
+
+        if ($user_id == -1) {
+            $this->println([
+                'success' => false,
+                'message' => 'You must be logged in to like a server!'
+            ]);
+            return false;
+        }
+
+        $like = Likes::getLike($server->id, $user_id);
+
+        if ($like) {
+            $this->println([
+                'success' => false,
+                'message' => 'You have already liked this server!'
+            ]);
+            return false;
+        }
+
+        $like = new Likes;
+        $like->setServerId($server->id);
+        $like->setUserId($user_id);
+
+        if (!$like->save()) {
+            $this->println([
+                'success' => false,
+                'message' => 'An error occurred: '.$like->getMessages()[0]
+            ]);
+            return true;
+        }
+
+        $this->println([
+            'success' => true,
+            'message' => 'Your like has been recorded. Thank you!',
+            'count' => Likes::getLikes($server->id)
+        ]);
+        return true;
+    }
+
     public function removeimageAction() {
         $this->view->setRenderLevel(View::LEVEL_NO_RENDER);
 
@@ -421,150 +551,6 @@ class ServersController extends BaseController {
         }
 
         $this->printStatus(true, $upload['link']);
-        return true;
-    }
-
-
-    public function statsAction() {
-        $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
-
-        if (!$this->request->isAjax()) {
-            return false;
-        }
-
-        $this->view->mostLiked = Likes::getMostLiked();
-        $this->view->newest    = Servers::getNewestServers();
-        $this->view->mostVotes = Servers::getMostVotedOn();
-        return true;
-    }
-
-    public function discordAction() {
-        $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
-
-        if (!$this->request->isPost() ||! $this->request->isAjax()) {
-            return false;
-        }
-
-        $server_id = $this->request->getPost("server_id", "int");
-
-        $discord = new Discord($server_id);
-        $discord->fetch();
-
-        $data = $discord->getRawData();
-
-        if (isset($data->code)) {
-            $this->flash->error("Error loading Discord: ".$data->message);
-        } else {
-            $this->view->discord = $discord;
-        }
-        return true;
-    }
-
-    public function reportAction() {
-        if (!$this->request->isPost() /*|| !$this->security->checkToken()*/) {
-            return $this->response->redirect("");
-        }
-
-        $user_id  = $this->getUser() ? $this->getUser()->id : null;
-        $username = $this->getUser() ? $this->getUser()->username : null;
-        $serverId = $this->request->getPost("serverId", "int");
-        $comment  = nl2br($this->request->getPost("comment", 'string'));
-
-        $server = Servers::getServer($serverId);
-
-        if (!$server) {
-            $this->dispatcher->forward([
-                'controller' => 'errors',
-                'action' => 'show404'
-            ]);
-            return true;
-        }
-
-        $this->view->seo_title = Servers::genSeoTitle($server);
-        $this->view->server = $server;
-
-        $lastReport = Reports::getRecentReport($user_id, $serverId);
-
-        if ($lastReport) {
-            $this->view->saved = false;
-            $this->view->error = "You have already submitted a report on this server within the last 5 minutes. Take a chill pill.";
-            return true;
-        }
-
-        $report = new Reports;
-        $report->setUserId($user_id);
-        $report->setUsername($username);
-        $report->setServerId($server->id);
-        $report->setReason($comment);
-        $report->setDateSubmitted(time());
-
-        if ($report->save()) {
-            $this->view->saved = true;
-        } else {
-            $this->view->saved = false;
-            $this->view->error = $report->getMessages()[0];
-        }
-    }
-
-    public function likeAction() {
-        $this->view->setRenderLevel(View::LEVEL_NO_RENDER);
-
-        if (!$this->request->isAjax()) {
-            $this->println([
-                'success' => false,
-                'message' => 'This page is available via ajax only!'
-            ]);
-            return false;
-        }
-
-        $serverId = $this->request->getPost("id", "int");
-        $server   = Servers::getServer($serverId);
-
-        if (!$server) {
-            $this->println([
-                'success' => false,
-                'message' => 'This server does not exist.'
-            ]);
-            return false;
-        }
-
-        $user_id = $this->getUser() ? $this->getUser()->id : -1;
-
-        if ($user_id == -1) {
-            $this->println([
-                'success' => false,
-                'message' => 'You must be logged in to like a server!'
-            ]);
-            return false;
-        }
-
-        $like = Likes::getLike($server->id, $user_id);
-
-        if ($like) {
-            $this->println([
-                'success' => false,
-                'message' => 'You have already liked this server!'
-            ]);
-            return false;
-        }
-
-        $like = new Likes;
-        $like->setServerId($server->id);
-        $like->setUserId($user_id);
-
-        if (!$like->save()) {
-            $this->println([
-                'success' => false,
-                'message' => 'An error occurred: '.$like->getMessages()[0]
-            ]);
-            return true;
-        }
-
-        $this->println([
-            'success' => true,
-            'message' => 'Your like has been recorded. Thank you!',
-            'count' => Likes::getLikes($server->id)
-        ]);
         return true;
     }
 
