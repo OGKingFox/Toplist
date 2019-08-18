92d80ec4c960e1fd6569b5d77c0012e0deac506f
KingFox <rune.evo2012@gmail.com>
2019-08-18 03:17:43 -0500

    Updated sample config.

--- public/config.sample.php
+++ public/config.sample.php
@@ -1,77 +1,85 @@
 <?php
-/**
- * Forces a timezone change incase your server isn't configured to your timezone
- */
-define("timezone", "America/Chicago");
-date_default_timezone_set(timezone);
 
-/**
- * Database credentials...self explanatory.
- */
-define("host",      "");
-define("username", 	"");
-define("password", 	"");
-define("dbname", 	"");
+$base_path = "/home/yoursite/public_html"; # an absolute path to your project's web root.
 
-/**
- * Discord OAuth Credentials
- * https://discordapp.com/developers/applications/
- */
-define('OAUTH2_CLIENT_ID', '');
-define('OAUTH2_CLIENT_SECRET', '');
-
-/**
- * OAuth Login URL. Edit with your own URL.
- */
-define("redirect_uri", "http://localhost/toplist/login/auth");
-
-define("auth_url", "https://discordapp.com/api/oauth2/authorize");
-define("token_url", "https://discordapp.com/api/oauth2/token");
-define("url_base", "https://discordapp.com/api/users/@me");
-
-/**
- * Webhook URL for the Bot
- */
-define("webhook_url", "");
-
-/**
- * For image uploads via the create/edit forms using QuillJS
- */
-define("IMGUR_KEY", "");
-
-/**
- * Google ReCaptcha Keys
- */
-define("CAPTCHA_PUBLIC", "");
-define("CAPTCHA_PRIVATE", "");
-
-/**
- * this is the folder you have it in. Should include starting and trailing slash.
- * if in root directory, should just be /
- */
-define("base_url",  "/");
-
-/**
- * sandbox or production
- */
-define("paypal_mode", "sandbox");
-
-/**
- * Create a PayPal app:
- * https://developer.paypal.com/developer/applications/create
- */
-define("sandbox_key", 	    "");
-define("production_key", 	"");
-
-/**
- * SMTP Settings
- * Enter your own SMTP server settings here or use the ones provided. doesnt matter to me :P
- */
-define("SMTP_HOST", 		"");
-define("SMTP_USER", 		"");
-define("SMTP_PASS", 		"");
-define("SMTP_SECURITY", 	"tls");
-define("SMTP_PORT", 		587);
-define("SMTP_FROM_EMAIL", 	"");
-define("SMTP_FROM_NAME", 	'');
-define("SMTP_HTML", 		true);
+$settings = [
+    'general' => [
+        'site_name' => 'Rune-Nexus' # Your sites name, not yet used.
+    ],
+    'database' => [
+        'host'     => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'dbname'   => '',
+    ],
+    'paypal' => [ # https://developer.paypal.com/docs/checkout/
+        'mode'       => 'sandbox',
+        'sandbox'    => '',
+        'production' => ''
+    ],
+    'recaptcha' => [ # https://www.google.com/recaptcha/intro/v3.html
+        'public'  => '',
+        'private' => ''
+    ],
+    'discord' => [ # https://discordapp.com/developers/docs/intro
+        'oauth' => [
+            'client_id'     => '',
+            'client_secret' => '',
+            'redirect_uri'  => 'http://localhost/login/auth',
+            'auth_url'      => 'https://discordapp.com/api/oauth2/authorize',
+            'token_url'     => 'https://discordapp.com/api/oauth2/token',
+            'url_base'      => 'https://discordapp.com/api/users/@me',
+        ],
+        'webhook'   => '', # webhook url for non-bot stuff.
+        'server_id' => '', # your server id.
+        'bot_key'   => '', # your bot's private key.
+    ],
+    'imgur' => [ # https://apidocs.imgur.com/?version=latest
+        'api_key' => ''
+    ],
+    'core' => [
+        'base_url'   => '/', # include trailing and ending/
+        'timezone'   => 'America/Chicago',
+        'cookie_key' => '', # a secure key for encrypting cookies. optimally 64 length.
+        'views' => [
+            'directory'  => $base_path.'/app/views/',
+            'cache_path' => $base_path.'/app/compiled/',
+            'templates'  => $base_path.'/app/compiled/templates/',
+            'extension'  => '.compiled',
+            'expires'    => 43200, # 12 hours
+        ],
+        'volt_options' => [
+            'compiledPath'      => $base_path.'/app/compiled/templates/',
+            'compiledExtension' => '.compiled',
+        ],
+        'paths' => [
+            $base_path.'/app/controllers/',
+            $base_path.'/app/models/',
+            $base_path.'/app/models/tools/',
+            $base_path.'/app/plugins/',
+            $base_path.'/Library/',
+        ],
+        'classes' => [
+            'PHPMailer'	    => $base_path.'/Library/PHPMailer/class.phpmailer.php',
+            'SMTP'	        => $base_path.'/Library/PHPMailer/class.smtp.php',
+            'CustomRouter'  => $base_path.'/app/CustomRouter.php',
+            'VoltExtension' => $base_path.'/app/VoltExtension.php',
+        ],
+        'files' => [
+            $base_path.'/Library/HTMLPurifier/HTMLPurifier.standalone.php',
+            $base_path.'/Library/discord/NexusBot.php',
+            $base_path.'/Library/discord/helpers/BotMessage.php',
+            $base_path.'/Library/discord/helpers/UserActions.php'
+        ],
+    ],
+    'smtp' => [ # SMTP Server Settings
+        'SMTP_HOST' => 'in-v3.mailjet.com',
+        'SMTP_USER' => '',
+        'SMTP_PASS' => '',
+        'SMTP_SECURITY' => 'tls',
+        'SMTP_PORT' => 587,
+        'SMTP_FROM_EMAIL' => '',
+        'SMTP_FROM_NAME' => 'Your site',
+        'SMTP_HTML' => true,
+    ]
+];
