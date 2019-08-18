2a10702b45e82daaecf7c4d85704dd6fd2f41ac9
KingFox <rune.evo2012@gmail.com>
2019-08-18 05:12:16 -0500

    cleaned up a bit

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,19 +8,15 @@ use Phalcon\Tag;
 class IndexController extends BaseController {
 
     public function indexAction() {
-        $articles = Articles::getArticles();
-
-        $this->view->articles = $articles;
-
-        $this->view->users   = Users::count();
-        $this->view->servers = Servers::count();
-        $this->view->votes   = Votes::count();
-        $this->view->likes   = Likes::count();
+        $this->view->articles = Articles::getArticles();
+        $this->view->users    = Users::count();
+        $this->view->servers  = Servers::count();
+        $this->view->votes    = Votes::count();
+        $this->view->likes    = Likes::count();
     }
 
     public function logoutAction() {
         $config = $this->getConfig();
-
         $this->cookies->set("access_token", '', time() - 1000, $config->path("core.base_url"));
         $this->session->destroy();
         return $this->response->redirect("");
