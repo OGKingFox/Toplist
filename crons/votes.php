6f6cde1b8d25e5f4d657b9c052187b6b7dc8197b
KingFox <rune.evo2012@gmail.com>
2019-09-01 18:12:52 -0500

    fixed time offset for calculating votes for the month.

--- crons/votes.php
+++ crons/votes.php
@@ -1,17 +1,17 @@
 <?php
-    $start = microtime(true);
-    include 'db_connect.php';
+$start = microtime(true);
+include 'db_connect.php';
 
-    $start = strtotime(date("Y-m-1 00:00:00"));
-    $end   = strtotime(date("Y-m-t 23:59:59"));
+$start = strtotime(date("Y-m-1 00:00:00"));
+$end   = strtotime(date("Y-m-t 23:59:59"));
 
-    $stmt = $pdo->prepare("UPDATE servers SET votes = (SELECT COUNT(*) FROM votes WHERE votes.server_id = servers.id AND votes.voted_on BETWEEN {$start} AND {$end})");
-    $stmt->execute();
-    $end = microtime(true);
+$stmt = $pdo->prepare("UPDATE servers SET votes = (SELECT COUNT(*) FROM votes WHERE votes.server_id = servers.id AND votes.voted_on-18000 BETWEEN {$start} AND {$end})");
+$stmt->execute();
+$end = microtime(true);
 
-    $updated = $stmt->rowCount();
-    $elapsed = number_format($end - $start, 4);
+$updated = $stmt->rowCount();
+$elapsed = number_format($end - $start, 4);
 
-    echo "[Success] Updated $updated Records in ".$elapsed."s! ";
-    //error_log(date('[m-d-y g:i A] ')."[Success] Updated $updated Records in ".$elapsed."s! ".PHP_EOL, 3, log_file);
+echo "[Success] Updated $updated Records in ".$elapsed."s! ";
+//error_log(date('[m-d-y g:i A] ')."[Success] Updated $updated Records in ".$elapsed."s! ".PHP_EOL, 3, log_file);
 ?>
