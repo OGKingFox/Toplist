144485b467119d1afeb28084a6d0af5838663ad9
KingFox <rune.evo2012@gmail.com>
2019-09-01 17:04:33 -0500

    fixed cron job not calculating votes for the month.

--- crons/votes.php
+++ crons/votes.php
@@ -2,7 +2,10 @@
     $start = microtime(true);
     include 'db_connect.php';
 
-    $stmt = $pdo->prepare("UPDATE servers SET votes = (SELECT COUNT(*) FROM votes WHERE votes.server_id = servers.id)");
+    $start = strtotime(date("Y-m-1 00:00:00"));
+    $end   = strtotime(date("Y-m-t 23:59:59"));
+
+    $stmt = $pdo->prepare("UPDATE servers SET votes = (SELECT COUNT(*) FROM votes WHERE votes.server_id = servers.id AND votes.voted_on BETWEEN {$start} AND {$end})");
     $stmt->execute();
     $end = microtime(true);
 
@@ -10,5 +13,5 @@
     $elapsed = number_format($end - $start, 4);
 
     echo "[Success] Updated $updated Records in ".$elapsed."s! ";
-    error_log(date('[m-d-y g:i A] ')."[Success] Updated $updated Records in ".$elapsed."s! ".PHP_EOL, 3, log_file);
+    //error_log(date('[m-d-y g:i A] ')."[Success] Updated $updated Records in ".$elapsed."s! ".PHP_EOL, 3, log_file);
 ?>
