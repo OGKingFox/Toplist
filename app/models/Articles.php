cb106ef372ef98b31638b067dc8b80fe159d0a27
KingFox <rune.evo2012@gmail.com>
2019-09-10 05:00:23 -0500

    changed icon, changed news limit to 3 on homepage

--- app/models/Articles.php
+++ app/models/Articles.php
@@ -31,6 +31,7 @@ class Articles extends Model {
             ])
             ->leftJoin("Users", "user.user_id = Articles.user_id", "user")
             ->orderBy("Articles.date_posted DESC")
+            ->limit(3)
             ->execute();
     }
 
