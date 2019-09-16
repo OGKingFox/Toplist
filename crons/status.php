21024b13f14e6d150eb9d44f26cabfb87a9263d3
KingFox <rune.evo2012@gmail.com>
2019-09-16 00:40:44 -0500

    server status cron job

---
+++ crons/status.php
@@ -0,0 +1,24 @@
+<?php
+$start = microtime(true);
+include 'db_connect.php';
+
+$stmt = $pdo->prepare("SELECT * FROM servers WHERE server_ip IS NOT NULL AND server_port != -1");
+$stmt->execute();
+$servers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($servers as $server) {
+    $host = $server['server_ip'];
+    $port = $server['server_port'];
+
+    $status = fsockopen($host, $port, $errno, $errstr, 1) ? 1 : 0;
+
+    $stmt = $pdo->prepare("UPDATE servers SET is_online = :status WHERE id = ".$server['id']);
+    $stmt->bindParam("status", $status);
+    $stmt->execute();
+
+    if (!$status) {
+        echo "Server is down.";
+    } else {
+        echo "Server is up.";
+    }
+}
